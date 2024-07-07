e46722ea49c796ed218aed90cdc11f17182cee31
kiety <53269602+kiety@users.noreply.github.com>
2024-07-06 23:26:07 -0600

    Improved responsiveness and header layout

--- resources/script.js
+++ resources/script.js
@@ -4,7 +4,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("name").style.fontSize = "2.5rem";
+    document.getElementById("header").style.boxShadow = "0 2px 4px 0 rgba(0, 0, 0, 0.12)";
   } else {
     document.getElementById("name").style.fontSize = "4.0rem";
+    document.getElementById("header").style.boxShadow = "0 2px 4px 0 rgba(0, 0, 0, 0.00)";
   }
 }

afb7741c0cff1f2127d839ef6075225a756e610b
kiety <53269602+kiety@users.noreply.github.com>
2024-07-07 00:39:45 -0600

    Further improved responsiveness.  Added 'Related Hobbies' section to site.

--- resources/script.js
+++ resources/script.js
@@ -4,9 +4,13 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("name").style.fontSize = "2.5rem";
+    document.getElementById("header-text-title").style.fontSize = "1.0rem";
+    document.getElementById("header-text-location").style.fontSize = "1.0rem";
     document.getElementById("header").style.boxShadow = "0 2px 4px 0 rgba(0, 0, 0, 0.12)";
   } else {
     document.getElementById("name").style.fontSize = "4.0rem";
+    document.getElementById("header-text-title").style.fontSize = "1.2rem";
+    document.getElementById("header-text-location").style.fontSize = "1.2rem";
     document.getElementById("header").style.boxShadow = "0 2px 4px 0 rgba(0, 0, 0, 0.00)";
   }
 }
